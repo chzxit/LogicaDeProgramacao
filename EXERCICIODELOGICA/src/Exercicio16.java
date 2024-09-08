97b5d43211b70c4c2fcaa4b473fc25a53da03748
Caio <ntjrio7@gmail.com>
2024-09-08 01:13:06 -0300

    Commit novo exercicio

--- EXERCICIODELOGICA/src/Exercicio16.java
+++ EXERCICIODELOGICA/src/Exercicio16.java
@@ -1,4 +1,21 @@
 
 public class Exercicio16 {
+	public static void main(String[] args) {
+		// TODO: Faça um algoritmo que calcule a quantidade de litros de combustível
+		// gastos em uma viagem, sabendo que o carro faz 12km com um litro. Deve-se
+		// fornecer ao usuário o tempo que será gasto na viagem a sua velocidade média,
+		// distância percorrida e a quantidade de litros utilizados para fazer a viagem.
+		// Fórmula: distância = tempo x velocidade.
+		// litros usados = distância / 12.
+		
+		int tempoViagem = 12;
+		int velocidadeMedia = 120;
+		int distancia = tempoViagem * velocidadeMedia;
+		double litrosUsados = distancia / 12;
+
+		System.out.println("O tempo que será gasto na viagem é de : " + tempoViagem + " Horas" + "\n");
+		System.out.println("A distância pecorrida é de: " + distancia + "Km" + "\n");
+		System.out.println("A quantidade de litros utilizados para fazer a viagem foi de: " + litrosUsados + " Litros");
+	}
 
 }
